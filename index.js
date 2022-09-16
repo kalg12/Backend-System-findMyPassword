7b206f32626b2d5c57867ebf8c5c29359c203978
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-09-16 12:59:55 -0500

    New configs

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Backend")
+const express = require('express');
+const app = express();
+const port = 4000;
+
+//Middleware
+app.use(express.json());
+
+//Routes
+app.use('/', (req,res) => {
+    res.send('Hello World');
+});
+
+//Ruta para obtener todos los usuarios de la base de datos
+app.get('/users', (req,res) => {
+    res.send('Obtener todos los usuarios');
+});
+
+
+//Listen
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
